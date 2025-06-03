a176514a4caf975d10e948a8f772de0871642cf2
Henrique William <os.henriquewilliam@gmail.com>
2025-06-02 23:54:48 -0300

    feat: Adding HTTP status response and message body for auth methods

--- src/main/java/com/momo_tech/momo_book/controllers/AuthController.java
+++ src/main/java/com/momo_tech/momo_book/controllers/AuthController.java
@@ -5,6 +5,8 @@ import com.momo_tech.momo_book.dto.RegisterRequest;
 import com.momo_tech.momo_book.entity.UserEntity;
 import com.momo_tech.momo_book.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,7 +24,8 @@ public class AuthController {
     private PasswordEncoder passwordEncoder;
 
     @PostMapping("/register")
-    public String register(@RequestBody RegisterRequest registerRequest) {
+    public ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest) {
+
         if (registerRequest.isValid() && registerRequest.isRegistrationValid()) {
             UserEntity userEntity = new UserEntity();
             userEntity.setName(registerRequest.getName());
@@ -31,25 +34,26 @@ public class AuthController {
             userEntity.setRole(registerRequest.getRole());
             userEntity.setPhoneNumber(registerRequest.getPhoneNumber());
             userRepository.save(userEntity);
-            return "User registered successfully!";
+            return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully! Welcome " + userEntity.getName() + ".");
         }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid data for registration, please check your input.");
 
-        return "Invalid data for register, please check your input.";
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+
         UserEntity userEntity = userRepository.findByEmail(loginRequest.getEmail())
                 .orElseThrow(() -> new RuntimeException(("User not found with email: " + loginRequest.getEmail())));
 
         if (loginRequest.isLoginValid() && loginRequest.isEmailValid()) {
             if (passwordEncoder.matches(loginRequest.getPassword(), userEntity.getPassword())) {
-                return "Login successful! Welcome " + userEntity.getName() + ".";
+                return ResponseEntity.status(HttpStatus.OK).body("Login successful! Welcome " + userEntity.getName() + ".");
             } else {
-                return "invalid credentials, please try again.";
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials, please check your email and password.");
             }
         }
-        return "Invalid data for login, please check your input.";
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid data for login, please check your input.");
     }
 
 }
